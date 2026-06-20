84329fc967c6bf343b1a9be7cf8be9d898b8b0be
root <r.touray@alustudent.com>
2026-06-20 10:03:48 -0700

    adding two strings using the f string on a text

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
+# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 str1 = "Holberton"
 str2 = "School"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print(f"Welcome to {str1} {str2}!")
 
 
