4cd309d948543788fa0192484c941586f90712ae
root <r.touray@alustudent.com>
2026-07-11 10:52:20 -0700

    Write an empty class Square that defines a square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+This module defines an empty Square class.
+It is documented according to PEP8 guidelines.
+"""
+
 class Square:
+    """
+    Square class (empty).
+    This class is documented according to PEP8 guidelines.
+    """
     pass
