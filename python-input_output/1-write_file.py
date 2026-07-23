6555cf55aed1ed21b8179afcbc24f5a827b8e7c2
root <r.touray@alustudent.com>
2026-07-23 07:09:22 -0700

    Create object from a JSON file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Function that writes a string to a text file (UTF8)
+and returns the number of characters written.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8).
+    Creates the file if it doesn't exist.
+    Overwrites the content if the file already exists.
+    Returns the number of characters written.
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
