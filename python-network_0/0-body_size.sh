30529dd6c8476160eb75802ecc0d3ea99c31ab59
root <r.touray@alustudent.com>
2026-07-28 05:33:22 -0700

    Python - Network

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# takes URL and displays the size of the response body
-curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
+# Script that takes in a URL, sends a request to that URL,
+# and displays the size of the body of the response in bytes
+
+curl -s "$1" | wc -c
+
