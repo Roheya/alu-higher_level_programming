d48fc5435acea66c10176c16986fe9090a5d648b
Roheya <r.touray@alustudent.com>
2026-07-31 05:28:13 -0700

    python almost a circle

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -1,7 +1,41 @@
+#!/usr/bin/python3
 import unittest
 from models.rectangle import Rectangle
 
 class TestRectangle(unittest.TestCase):
+    def test_valid_init(self):
+        r = Rectangle(1, 2)
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.height, 2)
+
     def test_area(self):
         r = Rectangle(3, 4)
         self.assertEqual(r.area(), 12)
+
+    def test_str(self):
+        r = Rectangle(4, 6, 2, 1, 99)
+        self.assertEqual(str(r), "[Rectangle] (99) 2/1 - 4/6")
+
+    def test_to_dictionary(self):
+        r = Rectangle(10, 2, 1, 9, 5)
+        d = r.to_dictionary()
+        self.assertEqual(d['width'], 10)
+        self.assertEqual(d['height'], 2)
+
+    def test_update_args(self):
+        r = Rectangle(10, 10, 10, 10)
+        r.update(89, 1, 2, 3, 4)
+        self.assertEqual(r.id, 89)
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.height, 2)
+        self.assertEqual(r.x, 3)
+        self.assertEqual(r.y, 4)
+
+    def test_update_kwargs(self):
+        r = Rectangle(10, 10, 10, 10)
+        r.update(id=89, width=1, height=2, x=3, y=4)
+        self.assertEqual(r.id, 89)
+        self.assertEqual(r.width, 1)
+        self.assertEqual(r.height, 2)
+        self.assertEqual(r.x, 3)
+        self.assertEqual(r.y, 4)
--- python-almost_a_circle/tests/test_square.py
+++ python-almost_a_circle/tests/test_square.py
@@ -1,7 +1,33 @@
+#!/usr/bin/python3
 import unittest
 from models.square import Square
 
 class TestSquare(unittest.TestCase):
+    def test_valid_init(self):
+        s = Square(5)
+        self.assertEqual(s.size, 5)
+
     def test_str(self):
         s = Square(5, 1, 2, 99)
         self.assertEqual(str(s), "[Square] (99) 1/2 - 5")
+
+    def test_to_dictionary(self):
+        s = Square(10, 2, 1, 5)
+        d = s.to_dictionary()
+        self.assertEqual(d['size'], 10)
+
+    def test_update_args(self):
+        s = Square(10, 10, 10)
+        s.update(89, 1, 2, 3)
+        self.assertEqual(s.id, 89)
+        self.assertEqual(s.size, 1)
+        self.assertEqual(s.x, 2)
+        self.assertEqual(s.y, 3)
+
+    def test_update_kwargs(self):
+        s = Square(10, 10, 10)
+        s.update(id=89, size=1, x=2, y=3)
+        self.assertEqual(s.id, 89)
+        self.assertEqual(s.size, 1)
+        self.assertEqual(s.x, 2)
+        self.assertEqual(s.y, 3)
